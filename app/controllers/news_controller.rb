87d392a86dda3d475a2ec7bcc2808c4b9ac1db88
diyu123 <694310292@qq.com>
2017-12-13 09:53:54 +0800

    图片前端展示

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,11 +2,11 @@ class NewsController < ApplicationController
 
 	# 企业新闻
 	def index
-		
+		@news = New.where(sort:2)
 	end
 
 	# 行业动态
 	def industry_news
-		
+		@trends = New.where(sort:1)
 	end
 end
